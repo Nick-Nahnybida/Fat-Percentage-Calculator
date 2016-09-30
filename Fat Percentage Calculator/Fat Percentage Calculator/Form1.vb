9b8da51c1a00676701b8268d1de4c9b509f1332d
Nick-Nahnybida <nicknahnybida@gmail.com>
2016-09-30 10:06:58 -0500

    Add files via upload

---
+++ Fat Percentage Calculator/Fat Percentage Calculator/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtCalories.Clear()
+        txtFat.Clear()
+        lblResult.Text = ("")
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim fat, cal, calFromFat, percentCalFromFat As Double
+        ' Dim testString As String = FormatPercent(percentCalFromFat)
+
+        ' Convert variables
+        fat = CDbl(txtFat.Text)
+        cal = CDbl(txtCalories.Text)
+
+        calFromFat = (fat * 9)
+
+        percentCalFromFat = calFromFat / cal
+        lblResult.Text = percentCalFromFat.ToString("P")
+
+    End Sub
+End Class
